4599e99946da0f95ab724a337b0af7581b05859a
deepika-cmd <72502466+deepika-cmd@users.noreply.github.com>
2020-11-18 20:42:13 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car_1)
     car2 = createSprite(300,200);
+   car2.addImage(car_2)
     car3 = createSprite(500,200);
+    car3.addImage(car_3)
     car4 = createSprite(700,200);
+    car4.addImage(car_4)
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,7 +47,8 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(rgb(198,135,103));
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
