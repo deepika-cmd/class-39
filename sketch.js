61e4be938491519eafc5e43b7f1a00ae5ca7a7a4
deepika-cmd <72502466+deepika-cmd@users.noreply.github.com>
2020-11-18 20:31:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var track , car_1 , car_2 , car_3 , car_4 ;
 
+function preload(){
+track = loadImage("images/track.jpg") 
+car_1 = loadImage("images/car1.png") 
+ car_2 = loadImage("images/car2.png") 
+  car_3= loadImage("images/car3.png") 
+  car_4 = loadImage("images/car4.png") 
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
